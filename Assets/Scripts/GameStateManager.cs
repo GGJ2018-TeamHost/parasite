93c4f71c570a15392f3a37b78db4b772ef26ea5f
QXC-LAPTOP\coldf_000 <cold.ferret@gmail.com>
2018-01-28 14:11:17 -0800

    level 3 is easier, added very small jump to mouse/elephant. Added end of game scene

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -11,4 +11,8 @@ public class GameStateManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
