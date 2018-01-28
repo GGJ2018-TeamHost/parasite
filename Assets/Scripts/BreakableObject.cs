fd8ac271fbe84466f419fd4a1e764965bf5753b7
QXC-LAPTOP\coldf_000 <cold.ferret@gmail.com>
2018-01-27 17:24:08 -0800

    Added elephant, breakable objects and elephants

---
+++ Assets/Scripts/BreakableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("ELEPHANT");
+        //Debug.Log(collision.collider.name);
+        if(collision.collider.name == "Elephant")
+        {
+            //Debug.Log("ELEPHANT NOT");
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
